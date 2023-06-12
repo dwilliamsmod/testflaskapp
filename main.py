54839a3b0ddc75cd845c70342b5a1e0d08d85555
dwilliamsmod <133213932+dwilliamsmod@users.noreply.github.com>
2023-06-12 15:09:04 +0100

    Update main.py
    commented flask code, added simple print message

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from flask import Flask
+# from flask import Flask
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-app.route("/")
+# app.route("/")
 
-def index():
-    return "Congratulations, you created a web app"
+# def index():
+#     return "Congratulations, you created a web app"
 
-if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8080, debug=True)
+# if __name__ == "__main__":
+#     app.run(host="127.0.0.1", port=8080, debug=True)
+
+print("hello worldies")
